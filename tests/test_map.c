1d6ab026c353fbaf93a285368477f63697056285
Alexandre Blondin Massé <alexandre.blondin.masse@gmail.com>
2020-06-26 19:53:38 -0400

    Ajoute test dans test_map

--- tests/test_map.c
+++ tests/test_map.c
@@ -8,10 +8,13 @@ int main () {
     ok(map->num_layers == 0, "empty map has 0 layer");
     diag("Adding a 5x6 layer");
     map_add_layer(map, 5, 6, 0, 0, 0);
-    diag("Adding a 8x4 layer with offset (1,2,3)");
-    map_add_layer(map, 8, 4, 1, 2, 3);
-    //printf("# map->num_layers = %d\n", map->num_layers);
-    //ok(map->num_layers == 2, "map now has 2 layers");
+    ok(map->num_layers == 1, "map now has 1 layer");
+    diag("Adding a 8x4 layer with offset (3,2,1)");
+    map_add_layer(map, 8, 4, 3, 2, 1);
+    diag("Adding a 4x3 layer with offset (1,2,3)");
+    map_add_layer(map, 4, 3, 1, 2, 3);
+    printf("# map->num_layers = %d\n", map->num_layers);
+    ok(map->num_layers == 3, "map now has 3 layers");
     diag("Printing the map's content");
     map_print(map, "# ");
     diag("Deleting the map");
