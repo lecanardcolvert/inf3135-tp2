70187b59ebceffd63c7af1e127355f806884093b
Alexandre Blondin Massé <alexandre.blondin.masse@gmail.com>
2020-06-29 15:41:08 -0400

    Ajoute module main

---
+++ src/main.c
@@ -0,0 +1,210 @@
+/**
+ * main.c
+ *
+ * Generate isometric map from a command line
+ *
+ * @author Alexandre Blondin Masse
+ */
+#include "isomap.h"
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <getopt.h>
+
+#define FORMAT_LENGTH 5
+#define FILENAME_LENGTH 200
+#define USAGE "\
+Usage: %s [-h|--help] [-s|--start X,Y,Z] [-e|--end X,Y,Z]\n\
+    [-w|--with-walk] [-f|--output-format FORMAT]\n\
+    [-i|--input-filename PATH] [-o|--output-filename PATH]\n\
+\n\
+Generate an isometric map from a JSON file. The file must respect\n\
+the right JSON format. See the README file for more details.\n\
+By default, read the file on stdin and write the result on stdout.\n\
+\n\
+Optional arguments:\n\
+  -h|--help                  Show this help message and exit.\n\
+  -s|--start X,Y,Z           The start location (X,Y,Z) in the map.\n\
+                             Default value is (0,0,0).\n\
+  -e|--end X,Y,Z             The end location (X,Y,Z) in the map.\n\
+                             Default value is (1,1,0).\n\
+  -w|--with-walk             Also display a shortest walk between\n\
+                             the start and end locations.\n\
+  -f|--output-format FORMAT  Select the ouput format (either text,\n\
+                             or png). The default format is text.\n\
+  -i|--input-filename PATH   Read the JSON file from the file PATH\n\
+                             If present, ignore stdin.\n\
+  -o|--output-filename PATH  Write the output to the file PATH.\n\
+                             Mandatory for the PNG output format.\n\
+                             If present, does not write on stdout.\n\
+"
+
+/**
+ * Parsing errors
+ */
+enum status {
+    ISOMAP_OK                                = 0,
+    ISOMAP_ERROR_FORMAT_NOT_SUPPORTED        = 1,
+    ISOMAP_ERROR_COORDINATES                 = 2,
+    ISOMAP_ERROR_PNG_FORMAT_WITHOUT_FILENAME = 3,
+    ISOMAP_ERROR_BAD_OPTION                  = 4,
+    ISOMAP_ERROR_INPUT_FILENAME_MANDATORY    = 5,
+};
+
+/**
+ * Arguments from the command line
+ */
+struct arguments {
+    bool show_help;                        // Show help?
+    bool with_walk;                        // Display walk?
+    int start_x;                           // The start location x-coordinate
+    int start_y;                           // The start location y-coordinate
+    int start_z;                           // The start location z-coordinate
+    int end_x;                             // The end location x-coordinate
+    int end_y;                             // The end location y-coordinate
+    int end_z;                             // The end location z-coordinate
+    char output_format[FORMAT_LENGTH];     // The output format
+    char input_filename[FILENAME_LENGTH];  // The input filename
+    char output_filename[FILENAME_LENGTH]; // The output filename
+    enum status status;                    // The status of the program
+};
+
+// Functions //
+// --------- //
+
+/**
+ * Retrieve the (x,y,z) coordinates from a string
+ *
+ * @param s  The string containing the coordinates
+ * @param x  The given layer
+ * @param y  The given row
+ * @param z  The given column
+ * @return   The status
+ */
+enum status parse_coordinates(const char *s, int *x, int *y, int *z) {
+    char tail = '\0';
+    int num_parsed = sscanf(s, "%d,%d,%d%c", x, y, z, &tail);
+    return num_parsed == 3 && tail == '\0' ? ISOMAP_OK : ISOMAP_ERROR_COORDINATES;
+}
+
+/**
+ * Print usage to a stream
+ *
+ * @param argv  The command line arguments
+ * @param file  The stream
+ */
+void print_usage(char *argv[], FILE *file) {
+    fprintf(file, USAGE, argv[0]);
+}
+
+/**
+ * Parse the command line arguments
+ *
+ * @param argc  The number of arguments
+ * @param argv  The arguments
+ * @return      The parsed arguments
+ */
+struct arguments parse_arguments(int argc, char *argv[]) {
+    struct arguments arguments = {
+        .show_help       = false,
+        .with_walk       = false,
+        .start_x         = 0,
+        .start_y         = 0,
+        .start_z         = 0,
+        .end_x           = 1,
+        .end_y           = 1,
+        .end_z           = 0,
+        .output_format   = "text",
+        .input_filename  = "",
+        .output_filename = "",
+        .status          = ISOMAP_OK
+    };
+    struct option long_opts[] = {
+        // Set flag
+        {"help",            no_argument,       0, 'h'},
+        {"with-walk",       no_argument,       0, 'w'},
+        // Don't set flag
+        {"start",           required_argument, 0, 's'},
+        {"end",             required_argument, 0, 'e'},
+        {"output-format",   required_argument, 0, 'f'},
+        {"input-filename",  required_argument, 0, 'i'},
+        {"output-filename", required_argument, 0, 'o'},
+        {0, 0, 0, 0}
+    };
+
+    while (true) {
+        int option_index = 0;
+        int c = getopt_long(argc, argv, "hws:e:f:i:o:", long_opts, &option_index);
+        if (c == -1) break;
+        switch (c) {
+            case 'h': arguments.show_help = true; break;
+            case 'w': arguments.with_walk = true; break;
+            case 's': arguments.status = arguments.status != ISOMAP_OK ? arguments.status :
+                                         parse_coordinates(optarg, &arguments.start_x,
+                                                           &arguments.start_y, &arguments.start_z);
+                      break;
+            case 'e': arguments.status = arguments.status != ISOMAP_OK ? arguments.status :
+                                         parse_coordinates(optarg, &arguments.end_x,
+                                                           &arguments.end_y, &arguments.end_z);
+                      break;
+            case 'f': strncpy(arguments.output_format, optarg, FORMAT_LENGTH - 1);
+                      break;
+            case 'i': strncpy(arguments.input_filename, optarg, FILENAME_LENGTH - 1);
+                      break;
+            case 'o': strncpy(arguments.output_filename, optarg, FILENAME_LENGTH - 1);
+                      break;
+            case '?': arguments.status = ISOMAP_ERROR_BAD_OPTION;
+                      break;
+        }
+    }
+
+    if (optind < argc) {
+        fprintf(stderr, "Error: option not recognized\n");
+        print_usage(argv, stderr);
+        exit(ISOMAP_ERROR_BAD_OPTION);
+    } else if (arguments.show_help) {
+        print_usage(argv, stdout);
+        exit(ISOMAP_OK);
+    } else if (arguments.status == ISOMAP_ERROR_COORDINATES) {
+        fprintf(stderr, "Error: the coordinates must be integers separated by commas\n");
+        print_usage(argv, stderr);
+        exit(ISOMAP_ERROR_COORDINATES);
+    } else if (strcmp(arguments.output_format, "text") != 0 &&
+               strcmp(arguments.output_format, "png")  != 0) {
+        fprintf(stderr, "Error: format %s not supported\n", arguments.output_format);
+        print_usage(argv, stderr);
+        exit(ISOMAP_ERROR_FORMAT_NOT_SUPPORTED);
+    } else if (strcmp(arguments.output_format, "png") == 0 &&
+               strcmp(arguments.output_filename, "")  == 0) {
+        fprintf(stderr, "Error: output filename is mandatory with png format\n");
+        print_usage(argv, stderr);
+        exit(ISOMAP_ERROR_PNG_FORMAT_WITHOUT_FILENAME);
+    }
+    return arguments;
+}
+
+int main(int argc, char *argv[]) {
+    struct arguments arguments = parse_arguments(argc, argv);
+    if (arguments.status == ISOMAP_OK) {
+        FILE *input = stdin;
+        if (strcmp(arguments.input_filename, "") != 0)
+            input = fopen(arguments.input_filename, "r");
+        struct isomap *isomap = isomap_create_from_json_file(input);
+        if (input != stdin)
+            fclose(input);
+        FILE *output = stdout;
+        if (strcmp(arguments.output_filename, "") != 0)
+            output = fopen(arguments.output_filename, "w");
+        if (strcmp(arguments.output_format, "text") == 0) {
+            isomap_print(output, isomap, "");
+            if (output != stdout)
+                fclose(output);
+        } else if (strcmp(arguments.output_format, "png") == 0) {
+            isomap_draw_to_png(isomap, arguments.output_filename);
+        }
+        isomap_delete(isomap);
+    }
+    return arguments.status;
+}
