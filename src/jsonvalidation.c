daca1be042e79b8f1033afa0850d002219c5c682
Hamel Bourdeau, Alexandre <hamel_bourdeau.alexandre@courrier.uqam.ca>
2020-07-29 17:32:14 -0400

    Correction: meilleure définition des fonctions
    jsonvalidation.h / jsonvalidation.c :
    * Les définitions des fonctions de validations ont été améliorées.
    * Des codes d'erreurs supplémentaires ont été ajoutés pour plus
    de précision.

--- src/jsonvalidation.c
+++ src/jsonvalidation.c
@@ -5,33 +5,90 @@
  */
 enum status {
     JSONVALIDATION_OK                           = 0,
-    JSONVALIDATION_ERROR                        = 1,
-    JSONVALIDATION_TOO_MANY_ROOT_OBJECTS        = 2,
-    JSONVALIDATION_MISSING_TILEWIDTH_KEY        = 3,
-    JSONVALIDATION_MISSING_ZOFFSET_KEY          = 4,
-    JSONVALIDATION_MISSING_TILESET_KEY          = 5,
-    JSONVALIDATION_MISSING_LAYERS_KEY           = 6,
-    JSONVALIDATION_BAD_TILEWIDTH_VALUE          = 7,
-    JSONVALIDATION_BAD_ZOFFSET_VALUE            = 8,
-    JSONVALIDATION_MISSING_TILE_ID_KEY          = 9,
-    JSONVALIDATION_MISSING_TILE_FILENAME_KEY    = 10,
-    JSONVALIDATION_MISSING_INCOMING_KEY         = 11,
-    JSONVALIDATION_MISSING_OUTGOING_KEY         = 12,
-    JSONVALIDATION_BAD_TILE_ID_VALUE            = 13,
-    JSONVALIDATION_BAD_TILE_FILENAME_VALUE      = 14,
-    JSONVALIDATION_BAD_TILE_INCOMING_VALUE      = 15,
-    JSONVALIDATION_BAD_TILE_OUTGOING_VALUE      = 16,
-    JSONVALIDATION_MISSING_LAYER_NUMROWS_KEY    = 17,
-    JSONVALIDATION_MISSING_LAYER_NUMCOLS_KEY    = 18,
-    JSONVALIDATION_MISSING_LAYER_OFFSET_KEY     = 19,
-    JSONVALIDATION_MISSING_LAYER_DATA_KEY       = 20,
-    JSONVALIDATION_BAD_LAYER_NUMROWS_VALUE      = 21,
-    JSONVALIDATION_BAD_LAYER_NUMCOLS_VALUE      = 22,
-    JSONVALIDATION_BAD_LAYER_OFFSET_VALUE       = 23,
-    JSONVALIDATION_BAD_LAYER_DATA_VALUE         = 24
+    JSONVALIDATION_OTHER_ERROR                  = 1,
+    JSONVALIDATION_MISSING_ISOMAP               = 2,
+    JSONVALIDATION_BAD_ISOMAP_VALUE             = 3,
+    
+    JSONVALIDATION_MISSING_ISOMAP_TILEWIDTH_KEY = 4,
+    JSONVALIDATION_BAD_ISOMAP_TILEWIDTH_VALUE   = 5,
+    JSONVALIDATION_MISSING_ISOMAP_ZOFFSET_KEY   = 6,
+    JSONVALIDATION_BAD_ISOMAP_ZOFFSET_VALUE     = 7,
+    JSONVALIDATION_MISSING_ISOMAP_TILESET_KEY   = 8,
+    JSONVALIDATION_BAD_ISOMAP_TILESET_VALUE     = 9,
+    JSONVALIDATION_MISSING_ISOMAP_LAYERS_KEY    = 10,
+    JSONVALIDATION_BAD_ISOMAP_LAYERS_VALUE      = 11,
+    
+    JSONVALIDATION_BAD_TILE_VALUE               = 12,
+    JSONVALIDATION_MISSING_TILE_ID_KEY          = 13,
+    JSONVALIDATION_BAD_TILE_ID_VALUE            = 14,
+    JSONVALIDATION_MISSING_TILE_FILENAME_KEY    = 15,
+    JSONVALIDATION_BAD_TILE_FILENAME_VALUE      = 16,
+    JSONVALIDATION_MISSING_TILE_INCOMING_KEY    = 17,
+    JSONVALIDATION_BAD_TILE_INCOMING_VALUE      = 18,
+    JSONVALIDATION_MISSING_TILE_OUTGOING_KEY    = 19,
+    JSONVALIDATION_BAD_TILE_OUTGOING_VALUE      = 20,
+    
+    JSONVALIDATION_BAD_LAYER_VALUE              = 21,
+    JSONVALIDATION_MISSING_LAYER_NUMROWS_KEY    = 22,
+    JSONVALIDATION_BAD_LAYER_NUMROWS_VALUE      = 23,
+    JSONVALIDATION_MISSING_LAYER_NUMCOLS_KEY    = 24,
+    JSONVALIDATION_BAD_LAYER_NUMCOLS_VALUE      = 25,
+    JSONVALIDATION_MISSING_LAYER_OFFSET_KEY     = 26,
+    JSONVALIDATION_BAD_LAYER_OFFSET_VALUE       = 27,
+    JSONVALIDATION_MISSING_LAYER_DATA_KEY       = 28,
+    JSONVALIDATION_BAD_LAYER_DATA_VALUE         = 29
 };
 
 // Validation functions //
 // -------------------- //
 
+const unsigned int jsonvalidation_validate_file(FILE *file) {
+}
 
+const unsigned int jsonvalidation_validate_root(json_t *json_root) {
+}
+
+const unsigned int jsonvalidation_validate_isomap(const json_t *isomap) {
+}
+
+const unsigned int jsonvalidation_validate_isomap_tilewidth(const json_t *isomap) {
+}
+
+const unsigned int jsonvalidation_validate_isomap_zoffset(const json_t *isomap) {
+}
+
+const unsigned int jsonvalidation_validate_isomap_tileset(const json_t *isomap) {
+}
+
+const unsigned int jsonvalidation_validate_tile(const json_t *tile) {
+}
+
+const unsigned int jsonvalidation_validate_tile_id(const json_t *tile) {
+}
+
+const unsigned int jsonvalidation_validate_tile_filename(const json_t *tile) {
+}
+
+const unsigned int jsonvalidation_validate_tile_incoming(const json_t *tile) {
+}
+
+const unsigned int jsonvalidation_validate_tile_outgoing(const json_t *tile) {
+}
+
+const unsigned int jsonvalidation_validate_isomap_layers(const json_t *layers) {
+}
+
+const unsigned int jsonvalidation_validate_layer(const json_t *layer) {
+}
+
+const unsigned int jsonvalidation_validate_layer_numrows(const json_t *layer) {
+}
+
+const unsigned int jsonvalidation_validate_layer_numcols(const json_t *layer) {
+}
+
+const unsigned int jsonvalidation_validate_layer_offset(const json_t *layer) {
+}
+
+const unsigned int jsonvalidation_validate_layer_data(const json_t *layer) {
+}
--- src/jsonvalidation.h
+++ src/jsonvalidation.h
@@ -20,154 +20,186 @@
  * 
  * @param  file  The input stream
  * @return 0 if the file is valid
- *         A number between 2 to 24 depending on the error
+ *         A number between 2 to 29 depending on the error (see jsonvalidation.c
+ *         for the error codes meanings) 
  */
-const unsigned int jsonvalidation_validate_file(const FILE *file);
+const unsigned int jsonvalidation_validate_file(FILE* file);
 
 /**
- * Validates that the JSON file has only 1 object in root
+ * Validates the root of the JSON file
+ * There must be only one 'isomap' object per file
  * 
  * @param  json_root The root of the JSON file
- * @return 0 if it has only 1 object in root
- *         2 if there is more than 1 object
+ * @return 0 if there are no errors,
+ *         2 if there is no 'isomap' object, 
+ *         3 if it is another type than an object
  */
-const unsigned int jsonvalidation_validate_single_root_key(json_t *json_root);
+const unsigned int jsonvalidation_validate_root(json_t *json_root);
 
 /**
- * Validates the data of the 4 mandatory keys :
- * 'tile-width', 'z-offset', 'tileset', 'layers'
+ * Validates the keys and values of the 'isomap' object
  * 
- * @param  json_root The root of the JSON file
- * @return 0 if the 4 keys are valid
- *         Either 4, 5, 8, 9 if there is an invalid key
+ * @param  isomap The root of the 'isomap' object
+ * @return 0 if the keys and values are valid
+ *         A number from 4 to 29 if something is invalid
  */
-const unsigned int jsonvalidation_validate_mandatory_keys(json_t *json_root);
+const unsigned int jsonvalidation_validate_isomap(const json_t *isomap);
 
 /**
- * Validates the 'tile-width' key. It is the width of a single tile
+ * Validates the 'tile-width' key of a 'isomap' object
  * It must be an int (usually a power of 2)
  * 
- * @param  json_root The root of the JSON file
- * @return 0 if tile-width is valid,
- *         7 if tile-width in invalid 
+ * @param  isomap The root of the 'isomap' object
+ * @return 0 if tile-width key and value are valid,
+ *         4 if tile-width key is missing, 
+ *         5 if tile-width value is invalid
  */
-const unsigned int jsonvalidation_validate_tilewidth(json_t *json_root);
+const unsigned int jsonvalidation_validate_isomap_tilewidth(const json_t *isomap);
 
 /**
- * Validates the 'z-offset' key. It is the vertical offset between two layers
+ * Validates the 'z-offset' key of a 'isomap' object
  * It must be an int
  * 
- * @param  json_root The root of the JSON file
- * @return 0 if z-offset is valid
- *         8 if z-offset is invalid
+ * @param  isomap The root of the 'isomap' object
+ * @return 0 if z-offset key and value are valid,
+ *         6 if z-offset key is missing, 
+ *         7 if z-offset value is invalid
  */
-const unsigned int jsonvalidation_validate_zoffset(const json_t *json_root);
+const unsigned int jsonvalidation_validate_isomap_zoffset(const json_t *isomap);
 
 /**
- * Validates the 'tileset' key and its 'tile' objects
- * It contains the information about the different tiles
+ * Validates the 'tileset' key of a 'isomap' object and its 'tile' objects
  * 
- * @param  json_tileset The root of the JSON tileset object array
- * @return              0 if all the tiles are valid
- *                      A number between 9 to 16 depending on the error
+ * @param  isomap The root of the 'isomap' object
+ * @return 0 if tileset and its 'tile' objects are valid,
+ *         8 if tileset key is missing,
+ *         9 if tileset value is invalid, 
+ *         A number from 12 to 20 if there is an error with a tile
  */
-const unsigned int jsonvalidation_validate_tileset(const json_t *json_tileset);
+const unsigned int jsonvalidation_validate_isomap_tileset(const json_t *isomap);
+
+/**
+ * Validates a 'tile' object of a 'tileset' object
+ * It must be an object
+ * 
+ * @param  tile The root of the JSON tile object
+ * @return 0 if tile keys and values are valid,
+ *         12 if tile value is invalid,
+ *         A number from 13 to 20 if there is an error with a key or value
+ */
+const unsigned int jsonvalidation_validate_tile(const json_t *tile);
 
 /**
  * Validates the 'id' key of a 'tile' object
  * It must be unique
  * 
- * @param  json_tile The root of the JSON tile object
- * @return           0 if id is valid
- *                   9 if key is missing, 13 if id is invalid
+ * @param  tile The root of the JSON tile object
+ * @return 0 if id key and value are valid,
+ *         13 if id key is missing,
+ *         14 if id value is invalid 
  */
-const unsigned int jsonvalidation_validate_tile_id(const json_t *json_tile);
+const unsigned int jsonvalidation_validate_tile_id(const json_t* tile)
+;
 
 /**
  * Validates the 'filename' key of a 'tile' object
  * If the path is relative then it must be relative of the directory
  * where the program is run
  * 
- * @param  json_tile The root of the JSON tile object
- * @return           0 if filename is valid
- *                   10 if key is missing, 14 if filename is invalid
+ * @param  tile The root of the JSON tile object
+ * @return 0 if filename key and value are valid,
+ *         15 if filename key is missing,
+ *         16 if filename value is invalid 
  */
-const unsigned int jsonvalidation_validate_tile_filename(const json_t *json_tile);
+const unsigned int jsonvalidation_validate_tile_filename(const json_t *tile);
 
 /**
  * Validates the 'incoming' key of a 'tile' object
- * It is a list of valid incoming moves from another tile
  * It must be of the [x,y,z] format. Each coordinate must be an int
  * 
- * @param  json_tile The root of the JSON tile object
- * @return           0 if incoming is valid
- *                   11 if key is missing, 15 if incoming is invalid
+ * @param  tile The root of the JSON tile object
+ * @return 0 if incoming key and values are valid,
+ *         17 if incoming key is missing,
+ *         18 if incoming value is invalid  
  */
-const unsigned int jsonvalidation_validate_tile_incoming(const json_t *json_tile);
+const unsigned int jsonvalidation_validate_tile_incoming(const json_t *tile);
 
 /**
  * Validates the 'outgoing' key of a 'tile' object
- * It is a list of valid outgoing moves to another tile
  * It must be of [x,y,z] format. Each coordinate must be an int
  * 
- * @param  json_tile The root of the JSON tile object
- * @return           0 if outgoing is valid
- *                   12 if key is missing, 16 if outgoing is invalid
+ * @param  tile The root of the JSON tile object
+ * @return 0 if outgoing key and values are valid,
+ *         19 if outgoing key is missing,
+ *         20 if outgoing value is invalid
  */
-const unsigned int jsonvalidation_validate_tile_outgoing(const json_t *json_tile);
+const unsigned int jsonvalidation_validate_tile_outgoing(const json_t *tile);
 
 /**
- * Validates the 'layers' key and its 'layer' objects
- * It contains the information about the different tiles
+ * Validates the 'layers' key of a 'isomap' object and its 'layer' objects
  * 
- * @param  json_layers The root of the JSON layers object array
- * @return             0 if all the layers are valid
- *                     A number between 17 to 24 depending on the error
+ * @param  isomap The root of the 'isomap' object
+ * @return 0 if layers and its 'layer' objects are valid,
+ *         10 if layers key is missing,
+ *         11 if layers value is invalid, 
+ *         A number from 21 to 29 if there is an error with a layer
  */
-const unsigned int jsonvalidation_validate_layers(const json_t *json_layers);
+const unsigned int jsonvalidation_validate_isomap_layers(const json_t *layers);
 
 /**
- * Validates the 'num-rows' key of a 'layer' object It is the number of rows of 
- * the layer
+ * Validates a 'layer' object of a 'layers' object
+ * It must be an object
+ * 
+ * @param  layer The root of the JSON layer object
+ * @return 0 if layer keys and values are valid,
+ *         21 if layer value is invalid,
+ *         A number from 22 to 29 if there is an error with a key or value
+ */
+const unsigned int jsonvalidation_validate_layer(const json_t *layer);
+
+/**
+ * Validates the 'num-rows' key of a 'layer' object
  * It must be an int
  * 
- * @param  json_layer The root of the JSON layer object
- * @return            0 if num-rows is valid
- *                    17 if key is missing, 21 if num-rows is invalid
+ * @param  layer The root of the JSON layer object
+ * @return 0 if num-rows key and values are valid,
+ *         22 if num-rows key is missing,
+ *         23 if num-rows value is invalid
  */
-const unsigned int jsonvalidation_validate_layer_numrows(const json_t *json_layer);
+const unsigned int jsonvalidation_validate_layer_numrows(const json_t *layer);
 
 /**
- * Validates the 'num-cols' key of a 'layer' object. Iis the number of columns
- * of the layer
+ * Validates the 'num-cols' key of a 'layer' object.
  * It must be an int
  * 
- * @param  json_layer The root of the JSON layer object
- * @return            0 if num-cols is valid
- *                    18 if key is missing, 22 if num-cols is invalid
+ * @param  layer The root of the JSON layer object
+ * @return 0 if num-cols key and values are valid,
+ *         24 if num-cols key is missing,
+ *         25 if num-cols value is invalid
  */
-const unsigned int jsonvalidation_validate_layer_numcols(const json_t *json_layer);
+const unsigned int jsonvalidation_validate_layer_numcols(const json_t *layer);
 
 /**
- * Validates the 'offset' key of a 'layer' object. It is the offset between 
- * this layer and the origin
+ * Validates the 'offset' key of a 'layer' object.
  * It must be of [x,y,z] format. Each coordinate must be an int
  * 
- * @param  json_layer The root of the JSON layer object
- * @return            0 if offset is valid
- *                    19 if key is missing, 23 if offset is invalid
+ * @param  layer The root of the JSON layer object
+ * @return 0 if offset key and values are valid,
+ *         26 if offset key is missing,
+ *         27 if offset value is invalid
  */
-const unsigned int jsonvalidation_validate_layer_offset(const json_t *json_layer);
+const unsigned int jsonvalidation_validate_layer_offset(const json_t *layer);
 
 /**
  * Validates the 'data' key of a 'layer' object. It is a unidimensional array
  * which contains the information about the tiles in the layer.
  * Each tile must either match with '0' or with a tile id
  * 
- * @param  json_layer The root of the JSON layer object
- * @return            0 if data is valid
- *                    20 if key is missing, 24 if data is invalid
+ * @param  layer The root of the JSON layer object
+ * @return 0 if data key and values are valid,
+ *         28 if data key is missing,
+ *         29 if data value is invalid
  */
-const unsigned int jsonvalidation_validate_layer_data(const json_t *json_layer);
+const unsigned int jsonvalidation_validate_layer_data(const json_t *layer);
 
 #endif
